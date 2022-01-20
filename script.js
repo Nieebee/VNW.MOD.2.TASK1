848d471ca325fbf56218cb24a09487613818c87b
Niédja Bezerra <niedjacarlos@gmail.com>
2022-01-19 21:38:52 -0300

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log ('Hello World')
+let name = 'Niedja';
+console.log("I'm Niedja")
+const birthYear = 1992;
+const actualYear = 2022;
+const subtraction = actualYear-birthYear;
+console.log('I am',subtraction,'years old')
+let quartaFeira = true;
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
